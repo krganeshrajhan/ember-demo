5986e1c721331f5eb7dfb0832a61218be3a6d984
krganeshrajhan <krganeshrajhan@gmail.com>
2016-02-12 17:26:05 +0800

    Save and display data

--- .tmp/scripts/combined-scripts.js
+++ .tmp/scripts/combined-scripts.js
@@ -9,6 +9,61 @@ var EmberDemo = window.EmberDemo = Ember.Application.create();
 
 (function() {
 
+EmberDemo.TodoController = Ember.ObjectController.extend({
+  isCompleted: function(key, value){
+    var model = this.get('model');
+
+    if (value === undefined) {
+      // property being used as a getter
+      return model.get('isCompleted');
+    } else {
+      // property being used as a setter
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted')
+});
+
+})();
+
+(function() {
+
+EmberDemo.ApplicationController = Ember.ArrayController.extend({
+  actions: {
+    createTodo: function() {
+      // Get the todo title set by the "New Todo" text field
+      var title = this.get('newTitle');
+      if (!title.trim()) { return; }
+
+      // Create the new Todo model
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false
+      });
+
+      // Clear the "New Todo" text field
+      this.set('newTitle', '');
+
+      // Save the new model
+      todo.save();
+    }
+  },
+
+remaining: function() {
+  return this.filterBy('isCompleted', false).get('length');
+}.property('@each.isCompleted'),
+
+inflection: function() {
+  var remaining = this.get('remaining');
+  return remaining === 1 ? 'item' : 'items';
+}.property('remaining')
+});
+
+})();
+
+(function() {
+
 EmberDemo.ApplicationAdapter = DS.FixtureAdapter;
 
 
